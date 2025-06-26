e39f603c5d85c2b25d27974c712ab69f4c9a2631
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2025-06-26 11:22:20 +0200

    add prisma session store library

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const path = require('node:path');
 const express = require('express');
 const session = require('express-session');
 const indexRouter = require('./routes/indexRouter');
+//session handling
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+
 require('dotenv').config();
 
 const app = express();
@@ -13,6 +18,26 @@ app.set('view engine', 'ejs');
 //setup styles
 const assetsPath = path.join(__dirname, 'public');
 app.use(express.static(assetsPath));
+
+//session handling
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000 
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUnitialized: true,
+        store: new PrismaSessionStore(
+            new PrismaClient(),
+            {
+                checkPeriod: 2*60*1000,
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            },
+        ),
+    }),
+);
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
